0a5c39e0bd191a19c43b4a2b0e04b87dd95b8ba3
catstyle1101 <anton.m.rus@gmail.com>
2023-07-18 17:59:23 +0500

    first commit

--- backend/api/exceptions.py
+++ backend/api/exceptions.py
@@ -1,11 +1,17 @@
 from rest_framework.views import exception_handler
 
+
 def custom_exception_handler(exc, context):
-    # Call REST framework's default exception handler first,
-    # to get the standard error response.
+    # Call the default exception handler first to get the standard error response
     response = exception_handler(exc, context)
 
     if response is not None:
-        response.data['status'] = response.status_code
-        response.data['exceptions'] = response.exception
+        # Check if the exception is a validation error
+        if response.status_code == 400 and 'detail' in response.data and isinstance(response.data['detail'], list):
+            # Customize the validation error response
+            response.data = {
+                'status': 'error',
+                'message': 'Validation failed',
+                'errors': response.data['detail']
+            }
     return response
