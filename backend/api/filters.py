6b763618a03051a3e399b0cfc03efdef146f9cbd
catstyle1101 <anton.m.rus@gmail.com>
2023-07-21 16:39:43 +0500

    readme

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -24,14 +24,22 @@ class ClientFilter(filters.FilterSet):
         q2 = Q(surname__istartswith=value)
         q3 = Q(surname__icontains=value)
         q4 = Q(name__icontains=value)
+        q5 = Q(children__name__icontains=value)
+        q6 = Q(children__surname__icontains=value)
+        q7 = Q(livingAddress__city__icontains=value)
+        q8 = Q(livingAddress__street__icontains=value)
         return queryset.filter(
-            q1 | q2
+            q1 | q2 | q3 | q4 | q5 | q6 | q7 | q8
         ).annotate(
             search_ordering=Case(
                 When(q1, then=Value(1)),
                 When(q2, then=Value(2)),
                 When(q3, then=Value(3)),
                 When(q4, then=Value(4)),
+                When(q5, then=Value(5)),
+                When(q6, then=Value(6)),
+                When(q7, then=Value(7)),
+                When(q8, then=Value(8)),
                 default=Value(100),
                 output_field=IntegerField(),
             )
